3bb44dcb42f908b42861e72c770505886acdff3b
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 06:08:13 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+/*
+public class Main
+{
+   public static void main(String[] args)
+   {
+      // Create a Point3D object that can hold Integers.
+      Point3D<Integer> point = new Point3D<>(10, 20, 30);
+
+      // Display the X and Ycoordinates stored in point.
+      System.out.println("Here are the X and Y coordinates.");
+      printPoint(point);
+   }
+
+   public static void printPoint(Point<?> point)
+   {
+      System.out.println("X Coordinate: " + point.getX());
+      System.out.println("Y Coordinate: " + point.getY());
+   }
+}
+*/
+
+
+
+
+/*
+public class Main
+{
+   public static void main(String[] args)
+   {
+      // Create a Point3D object that can hold Integers.
+      Point3D<Integer> point = new Point3D<>(10, 20, 30);
+
+      // Display the coordinates stored in point.
+      System.out.println("Here are the values in point.");
+      System.out.println("X Coordinate: " + point.getX());
+      System.out.println("Y Coordinate: " + point.getY());
+      System.out.println("Z Coordinate: " + point.getZ());
+   }
+}
+*/
